927bea7b1142f128c18ec6d882b663dbb95e90f4
Milivojevic MIlos <milivojevicm66@gmail.com>
2021-02-12 09:59:11 +0100

    keep getting compilation error, cant import static method get from mock ..

--- src/test/java/milos/sf/controllers/IndexControllerTest.java
+++ src/test/java/milos/sf/controllers/IndexControllerTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
@@ -14,6 +16,7 @@ import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 class IndexControllerTest {
 
@@ -29,6 +32,16 @@ class IndexControllerTest {
         MockitoAnnotations.openMocks(this);
         controller = new IndexController(recipeService);
     }
+
+    @Test
+    void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mockMvc.perform(get(forwardedUrlTemplate("/")))
+        .andExpect(status().isOk())
+        .andExpect(view().name("index"));
+    }
+
+
 
     @Test
     void getIndexPage() {
