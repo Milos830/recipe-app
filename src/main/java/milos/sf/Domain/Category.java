8248bb016546f9adcd778e1d1a55a4da663ca638
Milivojevic MIlos <milivojevicm66@gmail.com>
2021-02-25 10:06:28 +0100

    added recipe properties to show.html page

--- src/main/java/milos/sf/Domain/Category.java
+++ src/main/java/milos/sf/Domain/Category.java
@@ -2,11 +2,15 @@ package milos.sf.Domain;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.Set;
 
 @Data
+@Getter
+@Setter
 @EqualsAndHashCode(exclude = {"recipes"})
 @Entity
 public class Category {
